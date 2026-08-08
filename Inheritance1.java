502c86a0c8e7cda427a46c0cc50bf370846e15e8
ammkazi <amm.kazi@gmail.com>
2026-08-08 16:17:58 +0530

    inheritance

--- Inheritance1.java
+++ Inheritance1.java
@@ -11,14 +11,6 @@ class Animal {
         this.weight = weight;
         this.breed = breed;
     }
-
-    public void accept() {
-    }
-
-    public void display() {
-        System.out.println("Name is " + name);
-        System.out.println("Age is " + age);
-    }
 }
 
 class Dog extends Animal {
@@ -29,6 +21,12 @@ class Dog extends Animal {
         super(name, age, weight, breed);
         this.isFriendly = isFriendly;
     }
+
+    public String toString() {
+        return "Dog [name=" + name + ", age=" + age + ", weight=" + weight + ", breed=" + breed + ", isFriendly="
+                + isFriendly + "]";
+    }
+
 }
 
 class Tiger extends Animal {
@@ -39,14 +37,35 @@ class Tiger extends Animal {
         this.isManEater = isManEater;
     }
 
+    public String toString() {
+        return "Tiger [name=" + name + ", age=" + age + ", weight=" + weight + ", breed=" + breed + ", isManEater="
+                + isManEater + "]";
+    }
+}
+
+class Elephant extends Animal {
+    protected boolean inZoo;
+
+    public Elephant(String name, int age, double weight, String breed, boolean inZoo) {
+        super(name, age, weight, breed);
+        this.inZoo = inZoo;
+    }
+
+    @Override
+    public String toString() {
+        return "Elephant [name=" + name + ", age=" + age + ", weight=" + weight + ", breed=" + breed + ", inZoo="
+                + inZoo + "]";
+    }
+
 }
 
 public class Inheritance1 {
     public static void main(String[] args) {
         Dog obj1 = new Dog("Tommy", 4, 19.32, "Labrador", true);
         Tiger obj2 = new Tiger("Shera", 5, 99.21, "Bengal Tiger", false);
-
-        obj1.display();
-        obj2.display();
+        Elephant obj3 = new Elephant("Laila", 16, 47, "African", false);
+        System.out.println(obj1.toString());
+        System.out.println(obj2.toString());
+        System.out.println(obj3.toString());
     }
 }
